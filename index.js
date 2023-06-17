3a49f5953ba945c1dd5e9e2dd626c6c957f4e73b
karanja03 <wambuik03@gmail.com>
2023-06-17 09:54:14 +0300

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let theNames = ['Guadalupe','Ollie','Aki'];
+let outPutMessages = [];
+
+
+function writeCards(theNames, eventName){
+    for(let a=0; a<theNames.length; a++){
+        let message=`Thank you, ${theNames[a]}, for the wonderful ${eventName} gift!`;
+        outPutMessages.push(message);
+        
+    }
+    return outPutMessages;
+}
+console.log(message);
+
+
+
+function countDown(posNumber) {
+    while (posNumber >= 0) {
+      console.log(posNumber);
+      posNumber--;
+    }
+  }
